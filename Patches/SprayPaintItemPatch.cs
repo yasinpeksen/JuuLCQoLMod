20ad47a7a3b572045bf1f795f23eaabd8553aa47
Hurşit Yasin Pekşen <forgameprogrammer@gmail.com>
2024-03-19 16:22:20 +0300

    Add project files.

---
+++ Patches/SprayPaintItemPatch.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+
+
+namespace JuuQoLMod.Patches
+{
+    [HarmonyPatch(typeof(SprayPaintItem))]
+    internal class SprayPaintItemPatch
+    {
+
+        // Infinite Spray
+        [HarmonyPatch(nameof(SprayPaintItem.ItemActivate))]
+        [HarmonyPostfix]
+        static void ItemActivateFix(ref float ___sprayCanTank)
+        {
+            ___sprayCanTank = 1f;
+        }
+
+        // Color Change
+        [HarmonyPatch(nameof(SprayPaintItem.ItemInteractLeftRight))]
+        [HarmonyPostfix]
+        static void ItemInteractLeftRightFix(bool right, ref SprayPaintItem __instance, ref int ___sprayCanMatsIndex)
+        {
+            if (right && !(__instance.playerHeldBy == null))
+            {
+                ___sprayCanMatsIndex++;
+                if (___sprayCanMatsIndex >= __instance.sprayCanMats.Length) ___sprayCanMatsIndex = 0;
+
+                Debug.Log($"Color Index {___sprayCanMatsIndex}!");
+                __instance.sprayParticle.GetComponent<ParticleSystemRenderer>().material = __instance.particleMats[___sprayCanMatsIndex];
+                __instance.sprayCanNeedsShakingParticle.GetComponent<ParticleSystemRenderer>().material = __instance.particleMats[___sprayCanMatsIndex];
+            }
+        }
+
+
+        // Lower Audio Volume
+        [HarmonyPatch(nameof(SprayPaintItem.Start))]
+        [HarmonyPostfix]
+        static void StartFix(ref SprayPaintItem __instance)
+        {
+            __instance.sprayAudio.volume = 0.25f;
+        }
+
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+using JuuQoLMod.Patches;
+
+namespace JuuQoLMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class JuuQoLModBase : BaseUnityPlugin
+    {
+        private static JuuQoLModBase Instance;
+
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = new JuuQoLModBase();
+            }
+
+            // Log
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            harmony.PatchAll(typeof(JuuQoLModBase));
+            harmony.PatchAll(typeof(SprayPaintItemPatch));
+        }
+    }
+}
